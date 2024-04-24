4256a51c73aa41f8643e4e7c68ecff8a1cb3f18f
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-04-24 16:25:46 +0600

    Fix .env and firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: "G-MCBXBQ5Y5L"
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
